1bbf527bca6ee46b9d10f4045479a4247d253188
Shubham Kailas Deshmukh <87111500+shubhamdsk@users.noreply.github.com>
2021-07-09 14:55:29 +0530

    Source And Logic

---
+++ Basic Programs On Statements/2.Natural numbers in Reverse Order form N to 1/source.c
@@ -0,0 +1,28 @@
+//Q:- Write a C program to all natural numbers in reverse from N to 1 range using for loop
+
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    int i;
+    int start;
+    int end;
+
+    /* Input start and end limit from user */
+    printf("Enter starting value: ");
+    scanf("%d", &start);
+
+    printf("Enter end value: ");
+    scanf("%d", &end);
+
+    // Run loop from 'start' to 'end' and decrement by 1 in each iteration
+     
+    for(i=start; i>=end; i--)
+    {
+        printf("%d\n", i);
+    }
+
+    return 0;
+    getch();
+}
